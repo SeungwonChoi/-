52ddeebeae630b0174982bf51622efb3b9040f75
SeungwonChoi <35623289+SeungwonChoi@users.noreply.github.com>
2019-10-07 14:44:20 +0900

    Update 전화번호 목록.py

--- Coding Test Kit/Hash/전화번호 목록.py
+++ Coding Test Kit/Hash/전화번호 목록.py
@@ -1,10 +1,9 @@
 # https://programmers.co.kr/learn/courses/30/lessons/42577
 
 def solution(phone_book):
-    for i in range(len(phone_book)):
-        for j in range(len(phone_book)):
-            if i == j:
-                continue;
-            if phone_book[j].split(phone_book[i])[0] == "":
-                return False
+    phone_book.sort()
+    
+    for num_short, num_long in zip(phone_book, phone_book[1:]):
+        if num_long.startswith(num_short):
+            return False
     return True
