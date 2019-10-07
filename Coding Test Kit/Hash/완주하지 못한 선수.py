0e11626e40b19da918ad3cd71c4c3680f465d5dd
SeungwonChoi <35623289+SeungwonChoi@users.noreply.github.com>
2019-10-07 14:11:48 +0900

    Update 완주하지 못한 선수.py

--- Coding Test Kit/Hash/완주하지 못한 선수.py
+++ Coding Test Kit/Hash/완주하지 못한 선수.py
@@ -1,11 +1,7 @@
 # https://programmers.co.kr/learn/courses/30/lessons/42576
 
+import collections
+
 def solution(participant, completion):
-    participant.sort()
-    completion.sort()
-    
-    for i in range(len(completion)):
-        if (participant[i] != completion[i]):
-            return participant[i]
-    
-    return participant[len(participant) - 1]
+    counter_diff = collections.Counter(participant) - collections.Counter(completion)
+    return list(counter_diff.keys())[0]
