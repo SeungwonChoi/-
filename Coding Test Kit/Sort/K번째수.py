cf0b9bfd65e706b7d3e106d43163422760441407
SeungwonChoi <35623289+SeungwonChoi@users.noreply.github.com>
2019-10-08 12:02:11 +0900

    Update K번째수.py

--- Coding Test Kit/Sort/K번째수.py
+++ Coding Test Kit/Sort/K번째수.py
@@ -1,8 +1,4 @@
 # https://programmers.co.kr/learn/courses/30/lessons/42748
 
 def solution(array, commands):
-    answer = []
-    for command in commands:
-        start, end, position = command
-        answer.append(sorted(array[start - 1:end])[position - 1])
-    return answer
+    return list(map(lambda x : sorted(array[x[0] - 1 : x[1]])[x[2] - 1], commands))
