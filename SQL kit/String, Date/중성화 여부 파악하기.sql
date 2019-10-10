e4f13e4495dfc05cad5e42a91b9dfd2a09efe573
SeungwonChoi <35623289+SeungwonChoi@users.noreply.github.com>
2019-10-10 10:09:57 +0900

    Update 중성화 여부 파악하기.sql

--- SQL kit/String, Date/중성화 여부 파악하기.sql
+++ SQL kit/String, Date/중성화 여부 파악하기.sql
@@ -6,7 +6,7 @@ SELECT
     , (CASE WHEN 
         (SEX_UPON_INTAKE LIKE '%Neutered%') OR (SEX_UPON_INTAKE LIKE '%Spayed%') THEN 'O' 
         ELSE 'X'
-    END ) AS 중성화
+    END) AS 중성화
 FROM 
     ANIMAL_INS 
 ORDER BY 
