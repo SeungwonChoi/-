09150aec3f17aca5c7512134a374051daf4d5c16
SeungwonChoi <35623289+SeungwonChoi@users.noreply.github.com>
2019-10-12 22:48:05 +0900

    Update 모의고사.py

--- Coding Test Kit/완전탐색(Brute-force Search)/모의고사.py
+++ Coding Test Kit/완전탐색(Brute-force Search)/모의고사.py
@@ -9,13 +9,8 @@ def solution(answers):
     scores = [0] * len(patterns)
     
     for question_num, answer in enumerate(answers):
-        if answer == (question_num % 5) + 1:
-            scores[0] = scores[0] + 1
-        
-        if answer == patterns[1][question_num % 8]:
-            scores[1] = scores[1] + 1
-            
-        if answer == patterns[2][question_num % 10]:
-            scores[2] = scores[2] + 1
+        for i, pattern in enumerate(patterns):
+            if answer == pattern[question_num % len(pattern)]:
+                scores[i] += 1
     
     return [i + 1 for i, score in enumerate(scores) if score == max(scores)]
