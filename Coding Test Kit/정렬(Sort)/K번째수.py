718509914e5055f32a7ce494bc0c424445c438fc
SeungwonChoi <35623289+SeungwonChoi@users.noreply.github.com>
2019-10-12 22:50:48 +0900

    Update K번째수.py

--- Coding Test Kit/정렬(Sort)/K번째수.py
+++ Coding Test Kit/정렬(Sort)/K번째수.py
@@ -1,4 +1,4 @@
 # https://programmers.co.kr/learn/courses/30/lessons/42748
 
 def solution(array, commands):
-    return list(map(lambda x : sorted(array[x[0] - 1 : x[1]])[x[2] - 1], commands))
+    return list(map(lambda x : sorted(array[x[0] - 1:x[1]])[x[2] - 1], commands))
