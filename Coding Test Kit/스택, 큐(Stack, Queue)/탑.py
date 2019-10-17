797317a1ad18cdfd3e22acc559498fc38166994d
SeungwonChoi <35623289+SeungwonChoi@users.noreply.github.com>
2019-10-17 17:47:02 +0900

    Update 탑.py

--- Coding Test Kit/스택, 큐(Stack, Queue)/탑.py
+++ Coding Test Kit/스택, 큐(Stack, Queue)/탑.py
@@ -4,7 +4,6 @@ def solution(heights):
     answer = [0] * len(heights)
     for i in range(1, len(heights)):
         for j in range(i - 1, -1, -1):
-            print(i, j)
             if heights[j] > heights[i]:
                 answer[i] = j + 1
                 break;
