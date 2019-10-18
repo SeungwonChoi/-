1e01fbaf2866b90608221973027e7920b4a37c73
SeungwonChoi <35623289+SeungwonChoi@users.noreply.github.com>
2019-10-18 17:48:22 +0900

    Update 더 맵게.py

--- Coding Test Kit/힙(Heap)/더 맵게.py
+++ Coding Test Kit/힙(Heap)/더 맵게.py
@@ -1,13 +1,16 @@
 # https://programmers.co.kr/learn/courses/30/lessons/42626
 
-import math
+import heapq
 
 def solution(scoville, K):
     cnt = 0
 
-    while min(scoville) < K and len(scoville) >= 2:
-        scoville.sort()
-        scoville.insert(0, scoville.pop(0) + 2 * scoville.pop(0))
+    heap = []
+    for i in scoville:
+        heapq.heappush(heap, i)
+
+    while heap[0] < K and len(heap) >= 2:
+        heapq.heappush(heap, heapq.heappop(heap) + 2 * heapq.heappop(heap))
         cnt += 1
-        
-    return -1 if min(scoville) < K else cnt
+
+    return -1 if heap[0] < K else cnt
