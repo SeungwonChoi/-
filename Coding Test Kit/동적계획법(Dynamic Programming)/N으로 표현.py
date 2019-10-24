02ab72e6c3005bbd8e1cdbe8ecf86df26d0480b4
SeungwonChoi <35623289+SeungwonChoi@users.noreply.github.com>
2019-10-24 16:51:43 +0900

    Update N으로 표현.py

--- Coding Test Kit/동적계획법(Dynamic Programming)/N으로 표현.py
+++ Coding Test Kit/동적계획법(Dynamic Programming)/N으로 표현.py
@@ -3,15 +3,10 @@
 values_by_used_cnt = [set([]) for i in range(0, 9)]
 def BFS(N, used_cnt):
     idx = used_cnt - 1
-    
-    if idx < 0 or idx > 7:
-        return
-    if len(values_by_used_cnt[idx]) != 0:
-        return
-    
+    if idx < 0 or idx > 7 or len(values_by_used_cnt[idx]) != 0: return
     BFS(N, idx)
-    values_by_used_cnt[idx].add(eval(str(N) * used_cnt))
     
+    values_by_used_cnt[idx].add(eval(str(N) * used_cnt))
     for i in range(0, int(used_cnt / 2)):
         for val_1 in values_by_used_cnt[i]:
             for val_2 in values_by_used_cnt[idx - 1 - i]:
@@ -25,7 +20,5 @@ def BFS(N, used_cnt):
 def solution(N, number):
     for i in range(1, 9):
         BFS(N, i)
-        if number in values_by_used_cnt[i - 1]:
-            return i
-        
+        if number in values_by_used_cnt[i - 1]: return i
     return -1
