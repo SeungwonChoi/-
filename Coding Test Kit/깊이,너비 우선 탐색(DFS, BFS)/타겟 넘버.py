edd423b6e3cdcc777067ba42ed8ccbe502521b99
SeungwonChoi <35623289+SeungwonChoi@users.noreply.github.com>
2019-10-28 00:30:58 +0900

    Update 타겟 넘버.py

--- Coding Test Kit/깊이,너비 우선 탐색(DFS, BFS)/타겟 넘버.py
+++ Coding Test Kit/깊이,너비 우선 탐색(DFS, BFS)/타겟 넘버.py
@@ -1,7 +1,16 @@
 # https://programmers.co.kr/learn/courses/30/lessons/43165
 
 def solution(numbers, target):
+    length = len(numbers)
     answer = 0
-    for i in numbers:
+    
+    for i in range(0, pow(2, length)):
+    expression = ''
+    for j in range(0, length):
+        sign = format(i, '0' + str(length) + 'b')[j]
+        expression += '+' if sign == '0' else '-'
+        expression += str(numbers[j])
+    if eval(expression) == target:
+        answer += 1
         
     return answer
