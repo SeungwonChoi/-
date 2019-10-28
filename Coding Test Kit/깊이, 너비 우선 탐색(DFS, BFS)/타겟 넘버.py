917084ca8e5b05591cd08cbabeba8df5fc7245df
SeungwonChoi <35623289+SeungwonChoi@users.noreply.github.com>
2019-10-28 15:02:33 +0900

    Update 타겟 넘버.py

--- Coding Test Kit/깊이, 너비 우선 탐색(DFS, BFS)/타겟 넘버.py
+++ Coding Test Kit/깊이, 너비 우선 탐색(DFS, BFS)/타겟 넘버.py
@@ -1,17 +1,9 @@
 # https://programmers.co.kr/learn/courses/30/lessons/43165
 
 def solution(numbers, target):
-    length = len(numbers)
-    cnt = 0
-    
-    def DFS(idx):
-        if idx < length:
-            DFS(idx + 1)
-            numbers[idx] *= -1
-            DFS(idx + 1)
-        elif sum(numbers) == target:
-            nonlocal cnt
-            cnt += 1
-
-    DFS(0)
-    return cnt
+    if not numbers and target == 0 :
+        return 1
+    elif not numbers:
+        return 0
+    else:
+        return solution(numbers[1:], target-numbers[0]) + solution(numbers[1:], target+numbers[0])
