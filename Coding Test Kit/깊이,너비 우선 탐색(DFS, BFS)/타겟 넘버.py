f61c0650a0c6ab275ccf01c47f04040a7ff7a076
SeungwonChoi <35623289+SeungwonChoi@users.noreply.github.com>
2019-10-28 14:31:33 +0900

    Update 타겟 넘버.py

--- Coding Test Kit/깊이,너비 우선 탐색(DFS, BFS)/타겟 넘버.py
+++ Coding Test Kit/깊이,너비 우선 탐색(DFS, BFS)/타겟 넘버.py
@@ -5,12 +5,11 @@ def solution(numbers, target):
     answer = 0
     
     for i in range(0, pow(2, length)):
-    expression = ''
-    for j in range(0, length):
-        sign = format(i, '0' + str(length) + 'b')[j]
-        expression += '+' if sign == '0' else '-'
-        expression += str(numbers[j])
-    if eval(expression) == target:
-        answer += 1
+        expression = 0
+        for j in range(0, length):
+            sign = format(i, '0' + str(length) + 'b')[j]
+            expression += numbers[j] * (1 if sign == '0' else -1)            
+        if expression == target:
+            answer += 1
         
     return answer
