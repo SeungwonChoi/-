e725f5faef738b9147be4d722576af25c22cfe36
SeungwonChoi <35623289+SeungwonChoi@users.noreply.github.com>
2019-10-31 13:13:07 +0900

    Update 예산.py

--- Coding Test Kit/이분탐색/예산.py
+++ Coding Test Kit/이분탐색/예산.py
@@ -5,4 +5,4 @@ import math
 def solution(budgets, M):
     mean = M / len(budgets)
     gap_sum = sum([mean - i for i in budgets if i < mean])
-    return math.floor(mean + gap_sum / len([i for i in budgets if i > mean]))
+    return min(math.floor(mean + gap_sum / len([i for i in budgets if i > mean])), max(budgets))
